a65f562313b36705b32d3905059254822bdbbd5a
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-19 20:34:11 -0300

    feat: add font-family Barlow

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -8,8 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&display=swap" rel="stylesheet">
         <!-- Styles -->
         @vite(['resources/css/app.css', 'resources/scss/app.scss'])
     </head>
