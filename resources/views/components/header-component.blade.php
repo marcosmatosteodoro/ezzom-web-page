817fbc423957c343f5f81d4dc2f76cac5f272038
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-19 18:18:20 -0300

    feat: add navbar

--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -3,6 +3,51 @@
     <video id="root-video" autoplay loop muted controls>
       <source src="/video.mp4" type="video/mp4">
     </video>
+
+    <div id="root-menu">
+      <nav>
+        <div class="nav-content">
+          <a href="#">
+            <img id="logo" src="/logo.svg" alt="Logo" width="100">
+          </a>
+
+          <button class="button-expand" type="button" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div id="menu-content">
+            <ul id="menu-links" style="margin-right: 61px">
+              <li menu="menu1">
+                <a href="#">Menu 1</a>
+              </li>
+              <li menu="menu2">
+                <a href="#">Menu 2</a>
+              </li>
+              <li menu="menu3">
+                <a href="#">Menu 3</a>
+              </li>
+            </ul>
+
+            <button id="search-button">
+              <img src="/search.svg" alt="search" width="20px" height="20px">
+            </button>
+
+            <ul style="gap: 21px;">
+              <li>
+                <a href="#">
+                  <img src="/facebook.svg" alt="facebook" width="18px" height="18px">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="/linkedin.svg" alt="linkedin" width="18px" height="18px">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   </div>
 </header>
 
