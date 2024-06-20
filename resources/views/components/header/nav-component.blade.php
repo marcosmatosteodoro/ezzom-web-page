a4af0ef05776bc64be41f79a5a9e599a973760e2
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-20 09:31:47 -0300

    feat: add responsive search button

--- resources/views/components/header/nav-component.blade.php
+++ resources/views/components/header/nav-component.blade.php
@@ -4,23 +4,29 @@
       <img id="logo" src="/logo.svg" alt="Logo" width="100">
     </a>
 
-    <div class="p-relative">
-      <button id="button-expand" type="button" aria-expanded="false">
-        <img src="/menu.svg" alt="menu" width="18px" height="12px" />
+    <div class="d-flex">
+      <button class="search-button" type="button">
+        <img src="/search.svg" alt="search" width="26px" height="26px">
       </button>
 
-      <div id="menu-content">
-        <ul id="menu-links">
-          @each('components.menu-item-link-component', $menuItems, 'item')
-        </ul>
-        
-        <button id="search-button" type="button">
-          <img src="/search.svg" alt="search" width="20px" height="20px">
+      <div class="p-relative">
+        <button id="button-expand" type="button" aria-expanded="false">
+          <img src="/menu.svg" alt="menu" width="18px" height="12px" />
         </button>
-        
-        <ul id="menu-icons">
-          @each('components.menu-item-li-component', $liItems, 'item')
-        </ul>
+
+        <div id="menu-content">
+          <ul id="menu-links">
+            @each('components.menu-item-link-component', $menuItems, 'item')
+          </ul>
+          
+          <button class="search-button lg-hide" type="button">
+            <img src="/search-green.svg" alt="search green" width="20px" height="20px">
+          </button>
+          
+          <ul id="menu-icons">
+            @each('components.menu-item-li-component', $liItems, 'item')
+          </ul>
+        </div>
       </div>
     </div>
   </div>
