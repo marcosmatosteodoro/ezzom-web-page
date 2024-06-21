79c62d48fe103a22d581700e1be4249c60d25749
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-20 22:23:03 -0300

    feat: create create first-splite-item

--- resources/views/components/main/first-content-component.blade.php
+++ resources/views/components/main/first-content-component.blade.php
@@ -2,9 +2,11 @@
   <div id="splide" class="splide">
     <div class="splide__track">
       <ul class="splide__list">
-        <li class="splide__slide">split1</li>
-        <li class="splide__slide">split2</li>
-        <li class="splide__slide">split3</li>
+        @include("components.main.first-splide-item-component", ['title'=> 'Lorem Ipsum dolor Sit Amet Dolor lorem ipsum', 'date'=> '10 de Abril 2023', 'bg'=> 'bg-green', 'image'=> '/images/image1.jpeg' ])
+        @include("components.main.first-splide-item-component", ['title'=> 'Lorem Ipsum dolor Sit Amet Dolor lorem ipsum', 'date'=> '10 de Abril 2023', 'bg'=> 'bg-blue', 'image'=> '/images/image2.jpeg' ])
+        @include("components.main.first-splide-item-component", ['title'=> 'Lorem Ipsum dolor Sit Amet Dolor lorem ipsum', 'date'=> '10 de Abril 2023', 'bg'=> 'bg-green', 'image'=> '/images/image3.jpeg' ])
+        @include("components.main.first-splide-item-component", ['title'=> 'Lorem Ipsum dolor Sit Amet Dolor lorem ipsum', 'date'=> '10 de Abril 2023', 'bg'=> 'bg-red', 'image'=> '/images/image4.jpeg' ])
+        @include("components.main.first-splide-item-component", ['title'=> 'Lorem Ipsum dolor Sit Amet Dolor lorem ipsum', 'date'=> '10 de Abril 2023', 'bg'=> 'bg-yellow', 'image'=> '/images/image5.jpeg' ])
       </ul>
     </div>
   </div>
---
+++ resources/views/components/main/first-splide-item-component.blade.php
@@ -0,0 +1,20 @@
+<li class="splide__slide">
+  <div class="splide-group" style="background-image: url('{{ $image }}')"> 
+    <div class="splide-border {{ $bg }}"></div>  
+    <article>
+      <div class="splide-title">
+        <h3>
+          {{ $title }}
+        </h3>
+
+        <span>
+          {{ $date }}
+        </span>
+      </div>
+
+      <div class="splide-arrow">
+        <img src="/arrow-left.svg" alt="arrow left" width="20px" height="17.69px" />
+      </div>
+    </article> 
+  </div>
+</li>
