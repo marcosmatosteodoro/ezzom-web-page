c90023c4fc81c29f1eba6e140fc8405d5071c32b
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-21 17:51:18 -0300

    create migrations

---
+++ database/migrations/2024_06_21_203138_create_news_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->boolean('headline')->default(false);
+            $table->unsignedBigInteger('category_id');
+            $table->string('cover', 255);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
