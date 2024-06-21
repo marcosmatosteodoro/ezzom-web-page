086728804414f164e5bb5d7e8c74584aa8ce9f9c
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-21 11:21:56 -0300

    feat: initial create grid news

---
+++ resources/views/components/main/card-news-component.blade.php
@@ -0,0 +1,7 @@
+<div class="card-news">
+  <div class="grid-news">
+    <div class="hr-vertical"></div>
+    <div class="content"></div>
+    <div class="carrossel"></div>
+  </div>
+</div>
--- resources/views/components/main/second-content-component.blade.php
+++ resources/views/components/main/second-content-component.blade.php
@@ -1,3 +1,7 @@
 <section id="second-content">
-
+  <div id="card-news-group">
+    @include('components.main.card-news-component')
+    @include('components.main.card-news-component')
+    @include('components.main.card-news-component')
+  </div>
 </section>
