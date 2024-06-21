e094b9c7a7cfc3b34d198870e02313b9437548c8
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-21 11:22:33 -0300

    feat: create hr-color

--- resources/views/components/main/card-news-component.blade.php
+++ resources/views/components/main/card-news-component.blade.php
@@ -1,6 +1,8 @@
 <div class="card-news">
   <div class="grid-news">
-    <div class="hr-vertical"></div>
+    <div class="hr-vertical">
+      <div class="hr-color hr-bg-h-lg bg-green"></div>  
+    </div>
     <div class="content"></div>
     <div class="carrossel"></div>
   </div>
