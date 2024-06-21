5845b1c3feea57e64f9efd679e60863c7da1f60d
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-20 22:53:36 -0300

    feat: config splidejs

--- resources/js/controllers/first-content.controller.js
+++ resources/js/controllers/first-content.controller.js
@@ -1,9 +1,16 @@
 import Splide from '@splidejs/splide';
 
 document.addEventListener('DOMContentLoaded', function () {
-  new Splide('#splide', {
+  new Splide('#first-content-splide', {
     type   : 'loop',
-    perPage: 4,
+    perPage: 5,
     autoplay: true,
+    arrows: false,
+    breakpoints: {
+      1200: { perPage: 4 },
+      992:  { perPage: 3 },
+      768:  { perPage: 2 },
+      576:  { perPage: 1 },
+    },
   }).mount();
 });
--- resources/views/components/main/first-content-component.blade.php
+++ resources/views/components/main/first-content-component.blade.php
@@ -1,5 +1,5 @@
 <section id="first-content">
-  <div id="splide" class="splide">
+  <div id="first-content-splide" class="splide">
     <div class="splide__track">
       <ul class="splide__list">
         @include("components.main.first-splide-item-component", ['title'=> 'Lorem Ipsum dolor Sit Amet Dolor lorem ipsum', 'date'=> '10 de Abril 2023', 'bg'=> 'bg-green', 'image'=> '/images/image1.jpeg' ])
