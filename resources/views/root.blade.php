a54338a7951b4e5ab6a9c1f92d9bd4efb06222a0
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-23 12:48:14 -0300

    feat: add fivicon and title

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <title>ezoom</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" type="image/x-icon" href="https://ezoom.com.br/application/modules/comum/assets/img/icon/apple-touch-icon-72x72-precomposed.png">
         <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&display=swap" rel="stylesheet">
         <!-- Styles -->
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
