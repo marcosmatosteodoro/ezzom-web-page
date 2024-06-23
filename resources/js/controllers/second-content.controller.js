3f2afdefbde912c53efac5b51fbc59dc43d08a71
marcosmatosteodoro <mp.matos25@gmail.com>
2024-06-22 22:44:57 -0300

    fix: breakpoints

--- resources/js/controllers/second-content.controller.js
+++ resources/js/controllers/second-content.controller.js
@@ -3,19 +3,19 @@ import Splide from '@splidejs/splide';
 document.addEventListener('DOMContentLoaded', function () {
   const splides = document.getElementsByClassName('second-content-splide');
 
-  const slipdeCOnfig = {
+  const slipdeConfig = {
     type   : 'loop',
     perPage: 3,
     autoplay: true,
     arrows: false,
     breakpoints: {
-      1790: { perPage: 2 },
-      1540: { perPage: 1 },
+      1840: { perPage: 2 },
+      1200: { perPage: 1 },
     },
   }
 
   for (let i = 0; i < splides.length; i++) {
     const splide = splides[i];
-    new Splide(`#${splide.id}`, slipdeCOnfig).mount();
+    new Splide(`#${splide.id}`, slipdeConfig).mount();
   }
 });
